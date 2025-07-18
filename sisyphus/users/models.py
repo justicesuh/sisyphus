015ed9d1adc51c8b00cd67475fe3b33672e7b7c0
Justice Suh <justice.suh@gmail.com>
2025-07-18 18:14:36 -0400

    Create custom UserManager

--- sisyphus/users/models.py
+++ sisyphus/users/models.py
@@ -1,8 +1,28 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from sisyphus.core.models import UUIDMixin
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **kwargs):
+        if email is None:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        return self._create_user(email, password, **kwargs)
 
 
 class User(AbstractUser, UUIDMixin):
@@ -11,6 +31,8 @@ class User(AbstractUser, UUIDMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
