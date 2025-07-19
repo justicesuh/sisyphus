cfa8896e9e1d7bedf92aa55eb1042319ea0ee1b6
Justice Suh <justice.suh@gmail.com>
2025-07-18 21:30:30 -0400

    Add status field to Job model

--- sisyphus/jobs/models.py
+++ sisyphus/jobs/models.py
@@ -40,10 +40,30 @@ class Job(UUIDModel):
     ONSITE = 'onsite'
     REMOTE = 'remote'
 
+    INTERESTED = 'interested'
+    DISMISSED = 'dismissed'
+    APPLIED = 'applied'
+    REJECTED = 'rejected'
+    INTERVIEW = 'interview'
+    OFFER = 'offer'
+    ACCEPTED = 'accepted'
+    WITHDRAWN = 'withdrawn'
+
     FLEXIBILITY_CHOICES = (
         (HYBRID, HYBRID.capitalize()),
         (ONSITE, ONSITE.capitalize()),
         (REMOTE, REMOTE.capitalize()),
+    )
+
+    STATUS_CHOICES = (
+        (INTERESTED, INTERESTED.capitalize()),
+        (DISMISSED, DISMISSED.capitalize()),
+        (APPLIED, APPLIED.capitalize()),
+        (REJECTED, REJECTED.capitalize()),
+        (INTERVIEW, INTERVIEW.capitalize()),
+        (OFFER, OFFER.capitalize()),
+        (ACCEPTED, ACCEPTED.capitalize()),
+        (WITHDRAWN, WITHDRAWN.capitalize()),
     )
 
     company = models.ForeignKey(Company, related_name='jobs', on_delete=models.CASCADE)
@@ -59,6 +79,9 @@ class Job(UUIDModel):
     flexibility = models.CharField(max_length=6, choices=FLEXIBILITY_CHOICES, default=ONSITE)
     description = models.TextField(null=True, blank=True)
     raw_html = models.TextField(null=True, blank=True)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=INTERESTED)
+    date_applied = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
