c96653b24e77d0ace4d9f01549e3e0f26a5968d4
Justice Suh <justice.suh@gmail.com>
2025-07-18 22:04:30 -0400

    Configure logging

--- sisyphus/settings.py
+++ sisyphus/settings.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 from environ import Env
@@ -39,6 +40,45 @@ INSTALLED_APPS = [
     'sisyphus.ui',
     'sisyphus.users',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{levelname}] {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '[{levelname}] {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARN',
+            'propagate': False,
+        },
+        'sisyphus': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },        
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
