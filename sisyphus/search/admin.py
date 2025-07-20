b83066d5ae8f2a1b4f79e076e5198a579a0476b9
Justice Suh <justice.suh@gmail.com>
2025-07-19 22:18:37 -0400

    Customize search admin list display

--- sisyphus/search/admin.py
+++ sisyphus/search/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from sisyphus.core.admin import UUIDModelAdmin
 from sisyphus.search.models import Search
 
-admin.site.register(Search, UUIDModelAdmin)
+
+class SearchAdmin(UUIDModelAdmin):
+    list_display = ('__str__', 'location', 'flexibility', 'period', 'easy_apply', 'last_executed')
+    list_filter = ('is_active',)
+
+
+admin.site.register(Search, SearchAdmin)
