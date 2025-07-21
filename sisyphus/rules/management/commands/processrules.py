6a501265248e9bc228f6a11bb122a3460201efa0
Justice Suh <justice.suh@gmail.com>
2025-07-21 12:27:48 -0400

    Ban jobs from banned companies

--- sisyphus/rules/management/commands/processrules.py
+++ sisyphus/rules/management/commands/processrules.py
@@ -6,6 +6,10 @@ from sisyphus.rules.models import Rule
 
 class Command(BaseCommand):
     def handle(self, **options):
+        jobs = Job.objects.banned()
+        for job in jobs:
+            job.dismiss(f'{job.company.name} is banned.')
+
         rules = Rule.objects.all()
         for rule in rules:
             if rule.operator == Rule.CONTAINS:
