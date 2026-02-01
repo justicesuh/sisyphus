29d639ce423a5c7f741d19eb139559943d422a9b
Justice Suh <justice.suh@gmail.com>
2026-02-01 16:20:42 -0500

    Load environment variables

--- sisyphus/settings.py
+++ sisyphus/settings.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 
+from environ import Env
+
+env = Env(
+    ALLOWED_HOSTS=(list, []),
+    DEBUG=(bool, False),
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,12 +15,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bzn(gt^n0k6s*304@3=4ygo4xw@*7^&&l7xfx(49di%218uyg&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -61,14 +68,7 @@ WSGI_APPLICATION = 'sisyphus.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'postgres',
-        'PORT': 5432,
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-    }
+    'default': env.db_url('DATABASE_URL')
 }
 
 
