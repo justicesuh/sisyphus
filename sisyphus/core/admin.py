32166ed5700bc3353b037055feb9ebb8195de632
Justice Suh <justice.suh@gmail.com>
2026-02-01 19:33:54 -0500

    Display fields in admin

--- sisyphus/core/admin.py
+++ sisyphus/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class UUIDModelAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        fields = super().get_readonly_fields(request, obj)
+        return fields + tuple([field for field in ['created_at', 'updated_at'] if hasattr(self.model, field)])
+    
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        fields.remove('uuid')
+        fields.insert(0, 'uuid')
+        return fields
