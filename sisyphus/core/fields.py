82f53f5b7483d5aede7cdb6a6571e42849084b1c
Justice Suh <justice.suh@gmail.com>
2026-02-01 19:29:36 -0500

    Create UUIDModel

--- sisyphus/core/fields.py
+++ sisyphus/core/fields.py
@@ -1,3 +1,6 @@
+import uuid
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -10,4 +13,21 @@ class AutoCreatedField(models.DateTimeField):
 class AutoUpdatedField(models.DateTimeField):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('auto_now', True)
+        super().__init__(*args, **kwargs)
+
+
+class UUIDField(models.UUIDField):
+    def __init__(self, verbose_name=None, primary_key=False, version=4, editable=False, *args, **kwargs):
+        if version == 2:
+            raise ValidationError('UUID version 2 is not supported.')
+        
+        if version < 1 or version > 7:
+            raise ValidationError('UUID version is not valid.')
+        
+        default = getattr(uuid, f'uuid{version}')
+
+        kwargs.setdefault('verbose_name', verbose_name)
+        kwargs.setdefault('primary_key', primary_key)
+        kwargs.setdefault('default', default)
+        kwargs.setdefault('editable', editable)
         super().__init__(*args, **kwargs)
--- sisyphus/core/models.py
+++ sisyphus/core/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-from sisyphus.core.fields import AutoCreatedField, AutoUpdatedField
+from sisyphus.core.fields import AutoCreatedField, AutoUpdatedField, UUIDField
 
 
 class TimestampedMixin(models.Model):
     created_at = AutoCreatedField()
     updated_at = AutoUpdatedField()
 
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    uuid = UUIDField(_('UUID'))
+
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(TimestampedMixin, UUIDMixin):
     class Meta:
         abstract = True
