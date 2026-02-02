39b7025c7428944128366f88a61d909f0a74c870
Justice Suh <justice.suh@gmail.com>
2026-02-02 15:06:31 -0500

    Implement pagination

--- sisyphus/web/views.py
+++ sisyphus/web/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from sisyphus.jobs.models import Job
@@ -12,4 +13,6 @@ def index(request):
 @login_required
 def job_list(request):
     jobs = Job.objects.select_related('company', 'location').order_by('-date_posted')
-    return render(request, 'jobs/job_list.html', {'jobs': jobs})
+    paginator = Paginator(jobs, 25)
+    page = paginator.get_page(request.GET.get('page'))
+    return render(request, 'jobs/job_list.html', {'page': page})
