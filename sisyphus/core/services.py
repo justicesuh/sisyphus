27dfa356531a9c8ae77cfe273cdd4f2953a18b78
Justice Suh <justice.suh@gmail.com>
2026-02-03 14:42:22 -0500

    Turn into singleton using lru_cache

--- sisyphus/core/services.py
+++ sisyphus/core/services.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from django.conf import settings
 from openai import OpenAI as OpenAIClient
 
@@ -6,7 +8,7 @@ class OpenAI:
     def __init__(self):
         self.client = OpenAIClient(api_key=settings.OPENAI_API_KEY)
 
-    def chat(self, messages, model='gpt-4o', temperature=0.7, max_tokens=None, **kwargs):
+    def chat(self, messages, model='gpt-4o', temperature=0.2, max_tokens=None, **kwargs):
         response = self.client.chat.completions.create(
             model=model,
             messages=messages,
@@ -22,3 +24,8 @@ class OpenAI:
             messages.append({'role': 'system', 'content': system})
         messages.append({'role': 'user', 'content': prompt})
         return self.chat(messages, **kwargs)
+
+
+@lru_cache
+def get_openai():
+    return OpenAI()
