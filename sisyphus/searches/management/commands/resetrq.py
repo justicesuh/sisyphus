353ee1ed4fc33598f702c61a7f715c5a68846505
Justice Suh <justice.suh@gmail.com>
2026-02-10 15:41:45 -0500

    Add command to reset queue

---
+++ sisyphus/searches/management/commands/resetrq.py
@@ -0,0 +1,23 @@
+import django_rq
+from rq import Worker
+from rq.command import send_stop_job_command
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        q = django_rq.get_queue('default')
+        q.empty()
+        workers = Worker.all(q.connection)
+        for worker in workers:
+            if worker.get_current_job():
+                send_stop_job_command(q.connection, worker.get_current_job().id)
+            worker.request_stop()
+        finished = q.finished_job_registry
+        for job_id in finished.get_job_ids():
+            finished.remove(job_id, delete=True)
+        failed = q.failed_job_registry
+        for job_id in failed.get_job_ids():
+            failed.remove(job_id, delete=True)
+        
