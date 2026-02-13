f7f077b64cc0b8445b6ceac1a93a7af5743d1dc8
Justice Suh <justice.suh@gmail.com>
2026-02-13 13:40:42 -0500

    Add search to job admin

--- sisyphus/jobs/admin.py
+++ sisyphus/jobs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from sisyphus.core.admin import UUIDModelAdmin
 from sisyphus.jobs.models import Job, Location
@@ -8,5 +10,16 @@ admin.site.register(Location, UUIDModelAdmin)
 
 @admin.register(Job)
 class JobAdmin(UUIDModelAdmin):
-    list_display = ('title', 'user', 'company', 'status', 'date_posted')
+    list_display = ('title', 'user', 'company_link', 'status', 'date_posted_date')
     list_filter = ('user', 'status', 'flexibility')
+    search_fields = ('title', 'company__name')
+    show_facets = admin.ShowFacets.ALWAYS
+
+    @admin.display(description='company', ordering='company__name')
+    def company_link(self, obj):
+        url = reverse('admin:companies_company_change', args=[obj.company_id])
+        return format_html('<a href="{}">{}</a>', url, obj.company)
+
+    @admin.display(description='date posted', ordering='date_posted')
+    def date_posted_date(self, obj):
+        return obj.date_posted.date() if obj.date_posted else None
